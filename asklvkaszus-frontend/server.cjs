6f7a593aa1eb73562fdff745dba49092460e6fa9
Łukasz Lebiedziński <121642236+lvkaszus@users.noreply.github.com>
2025-06-16 00:51:58 +0200

    Express.js Fix: TypeError: Missing parameter name at 1

--- asklvkaszus-frontend/server.cjs
+++ asklvkaszus-frontend/server.cjs
@@ -10,7 +10,7 @@ const distPath = path.join(__dirname, 'dist');
 app.use(compression());
 app.use(express.static(distPath));
 
-app.get('*', (req, res) => {
+app.get('/*any', (req, res) => {
   res.sendFile(path.resolve(distPath, 'index.html'));
 });
 
