e63da2241b23506b270f359214d6dda1b9502d81
Łukasz Lebiedziński <121642236+lvkaszus@users.noreply.github.com>
2026-02-20 21:09:15 +0100

    Update dependencies to latest versions

--- asklvkaszus-backend/asklvkaszus/version.py
+++ asklvkaszus-backend/asklvkaszus/version.py
@@ -1,1 +1,1 @@
-backend_version = "3.2.2"
+backend_version = "3.2.3"
--- asklvkaszus-frontend/src/currentVersion.jsx
+++ asklvkaszus-frontend/src/currentVersion.jsx
@@ -1,5 +1,5 @@
 const currentVersion = () => {
-    const frontendVersion = "3.2.2";
+    const frontendVersion = "3.2.3";
 
     return frontendVersion;
 }
