8320a59eb280ffe52df0aac2d53bbdd3fc940231
Łukasz Lebiedziński <github@lvkasz.us>
2026-05-10 01:34:11 +0200

    Add frontend build script and update build command in package.json

---
+++ asklvkaszus-frontend/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "[Ask @lvkaszus! - Frontend: Pre-Builder]   Setting environment variables..."
+
+VITE_DOMAIN="${DOMAIN:-http://domain.tld}"
+VITE_YOUR_NICKNAME="${YOUR_NICKNAME:-@yourNickname}"
+
+echo "[Ask @lvkaszus! - Frontend: Pre-Builder]   Environment variables set!"
+echo "[Ask @lvkaszus! - Frontend: Pre-Builder]   Setting YOUR_NICKNAME inside index.html file..."
+
+sed -i "s/\[\[yourNickname\]\]/${YOUR_NICKNAME:-@me}/g" "index.html"
+
+echo "[Ask @lvkaszus! - Frontend: Pre-Builder]   YOUR_NICKNAME inside index.html file has been set!"
+echo "[Ask @lvkaszus! - Frontend: Pre-Loader]   Starting building source code files..."
+
+VITE_DOMAIN="$VITE_DOMAIN" VITE_YOUR_NICKNAME="$VITE_YOUR_NICKNAME" vite build
